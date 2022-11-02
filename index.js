1beb1fe7575dcc70c7546d11dc0123743cb2086d
Kyhou <joelskill@gmail.com>
2022-11-01 23:08:28 -0300

    Timestamp Microservice

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,9 +19,34 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/:time", function (req, res) {
+  var unix;
+  var utc;
+  var json;
+
+  if (req.params.time) {
+    var time = new Date(req.params.time);
+
+    if (!isNaN(time)) {
+      unix = Math.floor(time.getTime() / 1000);
+      utc = time.toUTCString();
+      json = { unix: unix, utc: utc };
+    } else if (!isNaN(new Date(req.params.time * 1000))) {
+      unix = Number(req.params.time);
+      utc = new Date(req.params.time * 1000).toUTCString();
+      json = { unix: unix, utc: utc };
+    } else {
+      json = { error: "Invalid Date" };
+    }
+  } else {
+    var time = new Date();
+    unix = Math.floor(time.getTime() / 1000);
+    utc = time.toUTCString();
+    json = { unix: unix, utc: utc };
+  }
+
+  res.json(json);
 });
 
 
